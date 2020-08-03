76c9cc194347b749ad2e43b7395e190f79b52c53
Marc Trojanowski <53346585+lux-g@users.noreply.github.com>
2020-08-03 13:57:32 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const clock = document.querySelector('.clock');
+const today = document.querySelector('.day');
+const months = document.querySelector('.month');
+const monthDate = document.querySelector('.month-date')
+
+function currentTime() {
+    const now = new Date();
+
+    const hours = now.getHours();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
+    const monthDay = now.getDate();
+    const year = now.getFullYear();
+
+    //Days
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const day = now.getDay();
+
+    //Months
+    const month = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const currentMonth = now.getMonth();
+
+    today.innerHTML = `${days[day]}`
+
+    months.innerHTML = `${month[currentMonth]}, ${monthDay}, ${year}`
+    clock.innerHTML = `${hours} : ${min} : ${sec}`;
+};
+
+setInterval(currentTime, 1000);
+
